0ec17a72817b3e3df8f404aac39f0d2443c780bd
Tim Oleson <timothy.oleson@gmail.com>
2021-08-06 11:23:56 -0400

    added more commands

--- Commands.ps1
+++ Commands.ps1
@@ -3,9 +3,6 @@
 #You can run the example with this command:
 kubectl apply -f https://kubernetes.io/examples/controllers/job.yaml
 
-
-
-
 #You can check the status with this with kubectl describe :
 kubectl describe jobs/pi
 
@@ -16,6 +13,34 @@ echo $pods
 #View the standard output of one of the pods:
 kubectl logs $pods
 
+################## JOBS IMPERATIVE DEMO  #############################
+# Imperative add space /bin/ sh to cause a failure
+kubectl create job counter --image=nginx -- /bin/ sh -c 'counter=0; while [ $counter -lt 3 ]; do counter=$((counter+1)); echo "$counter";  sleep 3; done;'
+
+#See Pods status see pods created due to failure
+kubectl get pods
+
+#Delete the job with issues to clean up pods
+kubectl delete job counter
+
+# Imperative fix issue with space /bin/sh that caused a failure
+kubectl create job counter --image=nginx -- /bin/sh -c 'counter=0; while [ $counter -lt 3 ]; do counter=$((counter+1)); echo "$counter";  sleep 3; done;'
+
+# See Pods status see pods
+kubectl get jobs
+# get pod name for logs
+kubectl get pods
+
+
+# view logs of pod running the job replace name with pod name for get pods command for counter job
+kubectl logs counter-rdnpp
+
+## Declaritive approach
+
+kubectl apply -f counterjob.yaml
+
+
+kubectl delete job counter
 ################## CRONJOB DEMO #############################
 
 
@@ -35,6 +60,12 @@ kubectl describe cronjob.batch/hello
 #View logs of cronjob:
 kubectl get Jobs -w
 
+# Get pods to view logs
+kubectl get pods
+
+# replace name with pod name for get pods command for cronjob
+kubectl logs hello-1628263140-5zlhg
+
 # Get one of the job runs pod logs has to be in the history of the cronjob :
 
 # Default history limit is 3, you can change it with:
@@ -48,6 +79,7 @@ kubectl logs hello-1628189160-jrl2j  #relace pod name with your pod name
 
 # We can watch the status of the cronjob with:
 kubectl get  cronjob.batch/hello -o wide
+
 # We can also watch the logs of the cronjob with
 kubectl get jobs --watch
 
